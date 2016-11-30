03c4db756fd34b5cb8cc22a831c435566f97cadf
Michael Mentele <Michaelrmentele@gmail.com>
2016-11-30 13:02:32 -0800

    testing play

--- spec/javascripts/app_spec.js
+++ spec/javascripts/app_spec.js
@@ -1,40 +1,40 @@
 describe('The MrelloApp object', function() {
-  it("is defined", function() {
+  xit("is defined", function() {
     expect(MrelloApp).toBeDefined();
   });
 
   describe('and namespaces constructors', function() {
-    it("the views", function() {
+    xit("the views", function() {
       expect(MrelloApp.views).toBeDefined();
     });
-    it("the models", function() {
+    xit("the models", function() {
       expect(MrelloApp.models).toBeDefined();
     });
-    it("the collections", function() {
+    xit("the collections", function() {x
       expect(MrelloApp.collections).toBeDefined();
     });
-    it("the routers", function() {
+    xit("the routers", function() {
       expect(MrelloApp.routers).toBeDefined();
     });
   });
 
   describe('and namespaces runtime attributes', function() {
-    it('the data', function() {
+    xit('the data', function() {
       expect(MrelloApp.data).toBeDefined();
     });
-    it('the app view', function() {
+    xit('the app view', function() {
       expect(MrelloApp.appView).toBeDefined();
     });
-    it('the app router', function() {
+    xit('the app router', function() {
       expect(MrelloApp.routes).toBeDefined();
     });
-    it('the event aggregator', function() {
+    xit('the event aggregator', function() {
       expect(MrelloApp.eventAggregator).toBeDefined();
     });
   });
 
   describe('#init', function() {
-    it('sets the app data to a Lists collection', function() {
+    xit('sets the app data to a Lists collection', function() {
       expect(MrelloApp.data).toEqual(new MrelloApp.collections.Lists());
     });
   });
--- spec/javascripts/models/card_spec.js
+++ spec/javascripts/models/card_spec.js
@@ -1,19 +1,39 @@
-describe("App.models.Card", function () {
-  it("has default values", function () {
-    var model = new MrelloApp.models.Card();
+describe("Card model", function () {
+  describe("creation", function() {
+    it("has default values", function () {
+      var model = new MrelloApp.models.Card()
 
-    expect(model).to.be.ok;
-    expect(model.get("title")).to.equal("Card");
-    expect(model.get("description")).to.equal("");
-  });
+      expect(model.get("title")).toEqual("Card")
+      expect(model.get("description")).toEqual("")
+    })
 
-  it("sets passed attributes", function () {
-    var model = new App.Models.Note({
-      title: "Grocery List",
-      text: "* Milk\n* Eggs\n*Coffee"
-    });
+    it("sets passed attributes", function () {
+      var model = new MrelloApp.models.Card({
+        title: "Card A",
+        description: "* Milk\n* Eggs\n*Coffee"
+      })
 
-    expect(model.get("title")).to.equal("Grocery List");
-    expect(model.get("text")).to.equal("* Milk\n* Eggs\n*Coffee");
-  });
-});
+      expect(model.get("title")).toEqual("Card A")
+      expect(model.get("description")).toEqual("* Milk\n* Eggs\n*Coffee")
+    })
+
+    describe("as part of a collection", function() {
+      beforeEach(function() {
+        // Should stub these dependencies
+        this.list = new MrelloApp.models.List({title: "Smart"})
+        this.list.cards.create({})
+      })
+
+      it("stores a reference to its parent list", function() {
+        debugger;
+        expect()
+      })
+
+      it("initializes comments collection", function() {
+
+      })
+
+      it("initializes cards collection")
+    })
+  })
+})
