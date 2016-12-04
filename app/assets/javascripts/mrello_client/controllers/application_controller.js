a6b31b96c10d657e4393b31dd8e6a22572493ca7
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-03 17:18:26 -0800

    updated rendering helpers

--- app/assets/javascripts/mrello_client/controllers/application_controller.js
+++ app/assets/javascripts/mrello_client/controllers/application_controller.js
@@ -1,15 +1,56 @@
 var MrelloApp = MrelloApp || {}
 
 MrelloApp.Controllers.Application = Backbone.Controller.extend({
-  containerID: "#app-container",
+  appContainerID:    "#app-container",
+  appHeaderID:    "#app-header",
+  appMessagesID:  "#app-messages",
+  appBodyID:      "#app-body",
 
-  renderPage: function(regions) {
-    this.clearAppView.apply(this)
-    $(this.containerID).append(regions.header.el)
-    $(this.containerID).append(regions.body.el)
+  // Catch non-existent routes
+  initialize: function() {
+    this.listenTo(MrelloApp.eventBus, "application:nonexistentRoute", this.nonexistentRoute)
   },
-  
-  clearAppView: function() {
-    $(this.containerID).empty()
+
+  nonexistentRoute: function() {
+    this.renderFlashMessage("That URL does not exist.")
+    this.redirectTo("home")
+  },
+
+  // Controller Helpers
+  checkValidSession: function() {
+    if ( MrelloApp.session.isNotAuthorized() ) {
+      this.renderFlashMessage("You are not authorized for that. Are you logged in?")
+      this.redirectTo("login")
+    }
+  },
+
+  // Render/Redirect Helpers
+  renderPage: function(regions) {
+    this.renderHeader(regions.header)
+
+    if(MrelloApp.hasFlash()) {
+      this.renderFlashMessage(MrelloApp.getAndClearFlash()) 
+    }
+
+    this.renderBody(regions.body)
+  },
+
+  renderHeader: function(headerView) {
+    $(this.appHeaderID).html(headerView.el)
+  },
+
+  renderFlashMessage: function(options) {
+    var flashConstructor = MrelloApp.Views.MessagesRegion.Flash
+    var flashView = flashConstructor(options.message, options.type)
+
+    return $(this.appMessagesID).html(flashView.el)
+  },
+
+  renderBody: function(bodyView) {
+    $(this.appBodyID).html(bodyView.el)
+  }
+
+  redirectTo: function(route) {
+    MrelloApp.eventBus.trigger("routes:go", route)
   },
 })
