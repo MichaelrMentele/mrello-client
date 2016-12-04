5330a77df14bc81ee0c89e91ad5b3cd097f8f0b9
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-03 17:29:52 -0800

    makes it eaiser to navigate and decouples from controllers

--- app/assets/javascripts/mrello_client/routers/mrello_router.js
+++ app/assets/javascripts/mrello_client/routers/mrello_router.js
@@ -1,51 +1,73 @@
 var MrelloApp = MrelloApp || {}
 
 MrelloApp.Routers.Router = Backbone.Router.extend({
-  
+
+  initialize: function() {
+    this.listenForNavigation()
+  },
+
+  listenForNavigation: function() {
+    this.listenTo(MrelloApp.dispatcher, "routes:go", this.go)
+  },
+
+  go: function(route) {
+    this.navigate(route, { trigger: true })
+  },
+
+  // URL Route Actions
   routes: {
-    ''                    :   'boardsControllerShow',
+    'home'                :   'boardsControllerShow',
+
     'register'            :   'usersControllerNew',
+
     'login'               :   'sessionsControllerNew',
-    'logout'              :   'sessionsControllerDestroy',
+
     'organizations'       :   'organizationsControllerIndex',
     'organizations/new'   :   'organizationsControllerNew',
     'organizations/show'  :   'organizationsControllerShow',
+
+    "*path"               :   'applicationControllerError'
   },
 
   boardsControllerShow: function() {   
     console.log("Router: @root, board#show...")
-    MrelloApp.boardsController.trigger("show")    
+    MrelloApp.eventBus.trigger("boards:show")    
   },
 
   usersControllerNew: function() {
     console.log("Router: @register, users#new"); 
-    MrelloApp.usersController.trigger("new");
+    MrelloApp.eventBus.trigger("users:new");
   },
 
   sessionsControllerNew: function() {
     console.log("Router: @login, sessions#new")
-    MrelloApp.sessionsController.trigger("new");
+    MrelloApp.eventBus.trigger("sessions:new");
   },
 
   sessionsControllerDestroy: function() {
     console.log("Router: @logout, sessions#destroy")
-    MrelloApp.sessionsController.trigger("destroy")
+    MrelloApp.eventBus.trigger("sessions:destroy")
   },
 
   organizationsControllerIndex: function() {
     console.log("Router: @index, organizations#index")
-    MrelloApp.organizationsController.trigger("index")
+    MrelloApp.eventBus.trigger("organizations:index")
   },
 
   organizationsControllerNew: function() {
     console.log("Router: @new, organizations#new")
-    MrelloApp.organizationsController.trigger("new")
+    MrelloApp.eventBus.trigger("organizations:new")
   },
 
   organizationsControllerShow: function() {
     console.log("Router: @show, organizations#show")
     // Must be an admin, show their board.
-    MrelloApp.organizationsController.trigger("show")      
+    MrelloApp.eventBus.trigger("organizations:show")      
+  },
+
+  applicationControllerError: function() {
+    console.log("Router: @error, application#error")
+    MrelloApp.eventBus.trigger("application:error")
   }
 })
 
