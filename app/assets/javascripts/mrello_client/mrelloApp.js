211a65bd7be07cf1c588373e87f1f6f30d180df6
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-05 19:32:38 -0800

    reorganizes views by UI, adds event bus, comments, flash object and helper methods

--- app/assets/javascripts/mrello_client/mrelloApp.js
+++ app/assets/javascripts/mrello_client/mrelloApp.js
@@ -1,53 +1,55 @@
 var MrelloApp = {
+  // Config
+  HOST_URL: "http://8b3f0ab5.ngrok.io", // http://mrello-api.herokuapp.com
 
   // constructor namespaces
   Models: {},      
   Collections: {}, 
   Views: {
-    Sessions: {},
-    Users: {},
-    Organizations: {},
-    Boards: {},
-    Lists: {},
-    Cards: {},
-    Checklists: {},
-    Comments: {},
+    HeaderRegions: {},
+    BodyRegions: {},
+    MessageRegions: {},
+    Composites: {},
+    Atomics: {},
   },     
   Routers: {}, 
   Controllers: {},
 
-  // application objects (called out for readability)
-  templates: HandlebarsTemplates,
-  data: {},               // Contains board data
-  organizations: {},      // TODO: should be namespaced under data
+  // Application objects (called out for readability)
+  templates: HandlebarsTemplates,            
   routes: {},             // Router
-  session: {},            // Singleton model containing session state
+  eventBus: {},           // Pub/Sub Bus
+  session: {},            // Singleton managing session token
+  currentUser: {}, //TODO: merge under session?
+  flash: {},
   
+  // Initializers
   init: function() {
     console.log("Mrello starting up...")
 
-    this.initializeState()
+    this.initializeEventBus()
     this.initializeControllers()
     this.initializeRouting()
-  },
-  
-  resetData: function() {
-    // TODO: Should be refactored so that lists are under a board Model
-    this.data = new this.Collections.Boards()
+
+    this.loadSession()
   },
 
-  clearCache: function() {
-    this.session.clear()
-    this.currentUser.clear()
+  initializeEventBus: function() {
+    this.eventBus = _.clone(Backbone.Events)
+    this.eventBus.on("all", function() {
+      console.log("Event published.")
+    })
+    return this.eventBus
   },
 
-  // Initializers
-  initializeState: function() {
+  loadSession: function() {
     this.session = new this.Models.Session()
     this.currentUser = new this.Models.User()
   },
 
   initializeControllers: function() {
+    // TODO: These are all singletons... why do I have constructors?
+    this.applicationController    = new this.Controllers.Application()
     this.sessionsController       = new this.Controllers.Sessions()
     this.usersController          = new this.Controllers.Users()
     this.boardsController         = new this.Controllers.Boards()
@@ -57,8 +59,37 @@ var MrelloApp = {
 
   initializeRouting: function() {
     this.routes = new this.Routers.Router() 
-    Backbone.history.start( { pushState: true } ) // pushState uses the full URL
-  }
+    Backbone.history.start( { pushState: true }) // pushState uses the full URL
+    return this.routes
+  },
+
+  // Flash Helpers
+  hasFlash: function() {
+    return !!this.flash.message
+  },
+
+  getAndClearFlash: function() {
+    var message = this.flash.message
+    var type = this.flash.type
+    this.flash = {}
+
+    return { message: message, type: type }
+  },
+
+  setFlash: function(message, type) {
+    this.flash.message = message
+    this.flash.type = type
+  },
+
+  // Data Helpers
+  resetData: function() {
+    this.boardsData = new this.Collections.Boards()
+  },
+
+  clearSessionCache: function() {
+    this.session.clear()
+    this.currentUser.clear()
+  },
 }
 
                                  
