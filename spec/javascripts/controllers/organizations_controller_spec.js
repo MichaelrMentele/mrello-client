bad22a43646d20597102930f952d6b4ed22efd00
Michael Mentele <Michaelrmentele@gmail.com>
2016-12-07 12:35:33 -0800

    skele

---
+++ spec/javascripts/controllers/organizations_controller_spec.js
@@ -0,0 +1,15 @@
+describe('Organizations controller', function() {
+  describe('index action', function() {
+    describe('users scope', function() {
+      it("gets the current users organizations from the server")
+    });
+
+    describe('default (all) scope', function() {
+      
+    });
+  });
+
+  describe('create action', function() {
+    it('')
+  });
+});
